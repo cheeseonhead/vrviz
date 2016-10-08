85722a96b47b832b88bdd3a236a55db404216621
Jeffrey Wu <cheeseonhead@gmail.com>
2016-10-08 16:27:21 -0400

    First

---
+++ Assets/Spectrum.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spectrum : MonoBehaviour {
+	
+	public GameObject prefab;
+	public int numberOfObjects = 20;
+	public int amplificationScale = 100;
+	public float radius = 5f;
+	public GameObject[] cubes;
+
+	void Start() {
+		for (int i = 0; i < numberOfObjects; i++) {
+			float angle = i * Mathf.PI * 2 / numberOfObjects;
+			Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+			Instantiate(prefab, pos, Quaternion.identity);
+		}
+		cubes = GameObject.FindGameObjectsWithTag ("cubes");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float[] spectrum = AudioListener.GetSpectrumData (1024, 0, FFTWindow.Hamming);
+		for (int i = 0; i < numberOfObjects; i++) {
+			Vector3 previousScale = cubes [i].transform.localScale;
+			previousScale.y = spectrum [i] * amplificationScale;
+			cubes [i].transform.localScale = previousScale;
+		}
+	}
+}
